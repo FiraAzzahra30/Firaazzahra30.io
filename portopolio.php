d85633659888857e33ee081cb9bcb98085d90c85
FiraAzzahra30 <firaazzhra13@gmail.com>
2025-02-01 09:48:47 +0700

    first commit

---
+++ portopolio.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+    <!--My CSS-->
+    <link rel="stylesheet" href="style.css"/>
+
+    <title>مرحبًا بالعالم!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>>
+</nav>
+ <!--akhir navbar -->
+
+  <!--jumbroton -->
+  <section class="jumbotron text-center">
+    <img src="img/chaeunwo.jpg" alt="chaeunwo" width="200" class="rounded-circle img-thumbnail"/> 
+  <h1 class="display-4">Cha Eun Woo</h1>
+  <p class="lead">pelajar conten kreator progremer.</p>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill=" #e2edff" fill-opacity="1" d="M0,32L34.3,37.3C68.6,43,137,53,206,90.7C274.3,128,343,192,411,192C480,192,549,128,617,90.7C685.7,53,754,43,823,69.3C891.4,96,960,160,1029,197.3C1097.1,235,1166,245,1234,240C1302.9,235,1371,213,1406,202.7L1440,192L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path></svg>
+ 
+</section>
+<!--akhir jumbroton-->
+<!--About -->
+<section id="about">
+  <div class="container">
+    <div class="row text-center m">
+      <div class="col">
+      <h2>about me</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center fs-5 text-center">
+    <div class="col-4">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed deleniti nam inventore quos quibusdam consequatur culpa itaque eaque reiciendis quisquam.</p>
+    </div>
+    <div class="col-4">
+      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim corrupti similique eligendi assumenda officiis tempore repudiandae aliquid accusantium dolorem, atque tenetur fugiat, veritatis aliquam voluptatum!</p>
+    </div>
+    </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill=" #e2edff" fill-opacity="1" d="M0,96L34.3,101.3C68.6,107,137,117,206,149.3C274.3,181,343,235,411,218.7C480,203,549,117,617,122.7C685.7,128,754,224,823,240C891.4,256,960,192,1029,186.7C1097.1,181,1166,235,1234,256C1302.9,277,1371,267,1406,261.3L1440,256L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
+</section>
+<!--akhir about -->
+
+<!--projects -->
+<section id="projects">
+  <div class="container">
+    <div class="row text-center">
+<div class="col">
+  <h2>my projects</h2>
+  </div>
+</div>
+<div class="row">
+<div class="col-4">
+<div class="card" >
+  <img src="img/projects.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+<div class="row">
+<div class="col-4">
+<div class="card" >
+  <img src="img/projects.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+<div class="row">
+<div class="col-4">
+<div class="card" >
+  <img src="img/projects.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+</div>
+  </div>
+</section>
+<!--akhir projects -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
